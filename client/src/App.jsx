316034f074164d04bff0bca130746281e1c0b5fc
sameeraherath <sameerastar6@gmail.com>
2025-03-08 10:37:05 +0530

    Fix bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import AppRoutes from "./routes/AppRoutes";
+import AppRoutes from "./routes/routes";
 
 function App() {
   return (
