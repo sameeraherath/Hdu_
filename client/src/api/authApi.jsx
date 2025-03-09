6d213224ce812537ab23d951342851934e5fba89
sameeraherath <sameerastar6@gmail.com>
2025-03-09 16:08:04 +0530

    Connect database for MySQL

--- client/src/api/authApi.jsx
+++ client/src/api/authApi.jsx
@@ -1,25 +1,17 @@
 import axios from "axios";
 
 export const register = async (userData) => {
-  // const response = await axios.post(
-  //   `${import.meta.env.VITE_API_URL}/api/auth/register`,
-  //   userData
-  // );
   const response = await axios.post(
-    `http://localhost:5001/api/auth/register`, // Add "http://"
+    `${import.meta.VITE_API_URL}/api/auth/register`,
     userData
   );
-  return response.data; // { token, role }
+  return response.data;
 };
 
 export const login = async (userData) => {
-  // const response = await axios.post(
-  //   `${import.meta.env.VITE_API_URL}/api/auth/login`,
-  //   userData
-  // );
-    const response = await axios.post(
-    `http://localhost:5001/api/auth/login`,
+  const response = await axios.post(
+    `${import.meta.VITE_API_URL}/api/auth/login`,
     userData
   );
-  return response.data; // { token, role }
+  return response.data;
 };
