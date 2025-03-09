a7fffa321399d21ff036064ff8b6c4accd3be5be
sameeraherath <sameerastar6@gmail.com>
2025-03-09 19:27:41 +0530

    Fix authApi bug

--- client/src/api/authApi.jsx
+++ client/src/api/authApi.jsx
@@ -1,17 +1,13 @@
 import axios from "axios";
 
+const BASE_URL = `${import.meta.env.VITE_API_URL}/api/auth`;
+
 export const register = async (userData) => {
-  const response = await axios.post(
-    `${import.meta.VITE_API_URL}/api/auth/register`,
-    userData
-  );
+  const response = await axios.post(`${BASE_URL}/register`, userData);
   return response.data;
 };
 
 export const login = async (userData) => {
-  const response = await axios.post(
-    `${import.meta.VITE_API_URL}/api/auth/login`,
-    userData
-  );
+  const response = await axios.post(`${BASE_URL}/login`, userData);
   return response.data;
 };
