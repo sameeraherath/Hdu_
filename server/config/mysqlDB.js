ef1e14316809cc9bd2d3d6310905258068f87ba1
sameeraherath <sameerastar6@gmail.com>
2025-03-09 18:25:47 +0530

    Fix bugs

---
+++ server/config/mysqlDB.js
@@ -0,0 +1,28 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.MYSQL_DATABASE,
+  process.env.MYSQL_USER,
+  process.env.MYSQL_PASSWORD,
+  {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    dialect: "mysql",
+    logging: false,
+  }
+);
+
+const connectMySql = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+    process.exit(1);
+  }
+};
+
+export { sequelize, connectMySql };
