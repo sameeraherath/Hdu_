cd191ee1a01f70bd567abceccdd17dcca1c3c662
sameeraherath <sameerastar6@gmail.com>
2025-03-10 00:00:46 +0530

    Add new functions

---
+++ server/models/PatientMySQL.js
@@ -0,0 +1,33 @@
+import { DataTypes } from "sequelize";
+
+const definePatient = (sequelize) => {
+  const PatientMySQL = sequelize.define(
+    "Patient",
+    {
+      id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+      fullName: { type: DataTypes.STRING, allowNull: false },
+      age: { type: DataTypes.INTEGER, allowNull: false },
+      birthDate: { type: DataTypes.DATEONLY, allowNull: false },
+      sex: {
+        type: DataTypes.ENUM("Male", "Female", "Other"),
+        allowNull: false,
+      },
+      condition: { type: DataTypes.TEXT, allowNull: false },
+      admitDateTime: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      contactDetails: { type: DataTypes.STRING, allowNull: false },
+      frequencyMeasure: {
+        type: DataTypes.ENUM("Red", "Green", "Blue", "Yellow", "Brown"),
+        allowNull: false,
+      },
+    },
+    { timestamps: true, tableName: "patients" }
+  );
+
+  return PatientMySQL;
+};
+
+export default definePatient;
